dc0a22654e36b77acfd41fd5a8b12e8bb6770d24
gdi2290 <github@gdi2290.com>
2013-12-21 18:15:55 -0800

    add koa-logger

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -1,25 +1,28 @@
+var logger = require('koa-logger');
 
- module.exports = function(app) {
+module.exports = function(app) {
+
+  app.use(logger());
 
   // Response time
-  app.use(function* responseTime(next){
-    var start = new Date;
-    yield next;
-    var ms = new Date - start;
-    this.set('X-Response-Time', ms + 'ms')
-  });
+  // app.use(function* responseTime(next){
+  //   var start = new Date;
+  //   yield next;
+  //   var ms = new Date - start;
+  //   this.set('X-Response-Time', ms + 'ms')
+  // });
 
   // Logger
-  app.use(function* logger(next) {
-    var start = new Date;
-    yield next;
-    var ms = new Date - start;
-    console.log('%s %s - %s', this.method, this.url, ms);
-  });
+  // app.use(function* logger(next) {
+  //   var start = new Date;
+  //   yield next;
+  //   var ms = new Date - start;
+  //   console.log('%s %s - %s', this.method, this.url, ms);
+  // });
 
   app.on('error', function(err, ctx){
     console.error('server error', err, ctx);
   });
 
- }
+}
 
