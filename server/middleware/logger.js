61b0e843749914545018a084d50b6af08f7edd52
gdi2290 <github@gdi2290.com>
2013-12-21 17:38:54 -0800

    add logger.js

---
+++ server/middleware/logger.js
@@ -0,0 +1,21 @@
+
+ module.exports = function(app) {
+
+  // Response time
+  app.use(function* responseTime(next){
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    this.set('X-Response-Time', ms + 'ms')
+  });
+
+  // Logger
+  app.use(function* logger(next) {
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    console.log('%s %s - %s', this.method, this.url, ms);
+  });
+
+ }
+
