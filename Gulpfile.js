a06f28e83209bbc67df038eaf4133a1dbc13a862
gdi2290 <github@gdi2290.com>
2014-01-12 17:20:49 -0800

    add stylus gulp task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,1 +1,9 @@
 var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+
+
+gulp.task('stylus', function(cb) {
+  gulp.src(paths.stylus.src)
+  .pipe(stylus())
+  .pide(gulp.dest(paths.stylus.dest));
+});
