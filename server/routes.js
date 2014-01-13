9d9e6e13b282d93a2d3a891f09c070400470e425
gdi2290 <github@gdi2290.com>
2014-01-12 21:31:51 -0800

    update routes to serve build files

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,13 @@
 var router = require('koa-route');
 var views = require('koa-render');
+var serve = require('koa-static');
 
 module.exports = function(app) {
   // set views engine
   app.use(views('./server/views', 'jade'));
+
+  // serve build files
+  app.use(serve(app.rootPath + '/build'));
 
   // render index
   app.use(router.get('/*', function *(next) {
@@ -13,4 +17,4 @@ module.exports = function(app) {
   }));
 
 
-}
+};
