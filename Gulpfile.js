b143550840af39cdd5f8027a874e2a526c776464
gdi2290 <github@gdi2290.com>
2014-01-12 17:21:57 -0800

    require path.json in Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var paths = require('./config/path.json');
 
 
 gulp.task('stylus', function(cb) {
@@ -7,3 +8,8 @@ gulp.task('stylus', function(cb) {
   .pipe(stylus())
   .pide(gulp.dest(paths.stylus.dest));
 });
+
+gulp.task('default', function(cb) {
+  gulp.run('stylus');
+});
+
