844fd9de96b325b06df6acbe0ce3c820be18431a
gdi2290 <github@gdi2290.com>
2014-01-12 17:37:43 -0800

    add livereload to Gulefile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,10 @@ var gulp   = require('gulp');
 var stylus = require('gulp-stylus');
 var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify');
+var refresh = require('gulp-livereload');
+var livereload = require('tiny-lr');
 
+var server = lr();
 var paths  = require('./config/path.json');
 
 gulp.task('browserify', function(cb) {
@@ -16,10 +19,16 @@ gulp.task('uglify', function(cb) {
     .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('stylus', function(cb) {
+gulp.task('styles', function(cb) {
   return gulp.src(paths.stylus.src)
   .pipe(stylus())
   .pide(gulp.dest(paths.stylus.dest));
+});
+
+gulp.task('server', function() {
+  server.listen(35729, function(err) {
+    if (err) return console.log(err);
+  });
 });
 
 gulp.task('default', function(cb) {
