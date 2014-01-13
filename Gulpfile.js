4ec51c8bf0c43fcab0f46543a279b7a662e780b3
gdi2290 <github@gdi2290.com>
2014-01-12 18:40:27 -0800

    update gulp paths with config.json

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,26 +11,23 @@ var server = livereload();
 var config  = require('./config/config.json');
 
 gulp.task('scripts', function(cb) {
-  return gulp.src(config.paths.scripts.src)
+  return gulp.src(config.paths.scripts)
     .pipe(browserify())
     .pipe(concat(config.build.scripts))
-    .pipe(gulp.dest(config.paths.scripts.dest))
+    .pipe(gulp.dest(config.build.path))
     .pipe(refresh(server));
 });
 
-gulp.task('uglify', function(cb) {
-  return gulp.src(['client/**/*.js'])
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
-});
-
 gulp.task('styles', function(cb) {
-  return gulp.src(config.paths.stylus.src)
+  return gulp.src(config.paths.stylus)
     .pipe(stylus({
       conpress: true
     }))
-    .pipe(gulp.dest(config.build.styles))
-    .pipe(gulp.dest(config.paths.stylus.dest));
+    .pipe(concat(config.build.styles))
+    .pipe(gulp.dest(config.build.path))
+    .pipe(refresh(server));
+});
+
 gulp.task('copy', function(cb) {
   gulp.src(config.paths.public)
     .pipe(gulp.dest(config.build.path))
@@ -41,6 +38,12 @@ gulp.task('lr-server', function(cb) {
   server.listen(config.livereload.port, function(err) {
     if (err) return console.log(err);
   });
+});
+
+gulp.task('uglify', function(cb) {
+  return gulp.src(config.paths.scripts)
+    .pipe(uglify())
+    .pipe(gulp.dest(config.build.path));
 });
 
 gulp.task('clean', function(cb) {
