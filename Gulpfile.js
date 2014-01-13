158bf89998c39f7e5ce3cbec08f947988013d5d5
gdi2290 <github@gdi2290.com>
2014-01-12 17:33:58 -0800

    add gulp-uglify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,23 @@
 var gulp   = require('gulp');
 var stylus = require('gulp-stylus');
+var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify');
 
 var paths  = require('./config/path.json');
+
 gulp.task('browserify', function(cb) {
   return gulp.task(['client/**/*.js'])
     .pipe(browserify());
 });
 
+gulp.task('uglify', function(cb) {
+  return gulp.src(['client/**/*.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
 gulp.task('stylus', function(cb) {
-  gulp.src(paths.stylus.src)
+  return gulp.src(paths.stylus.src)
   .pipe(stylus())
   .pide(gulp.dest(paths.stylus.dest));
 });
