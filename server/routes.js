066c503282c0c0550899db4725386c85e207b7bc
gdi2290 <github@gdi2290.com>
2014-02-01 03:08:58 -0800

    update routes with controller and todo

--- server/routes.js
+++ server/routes.js
@@ -2,13 +2,24 @@ var router = require('koa-route');
 var views = require('koa-render');
 var serve = require('koa-static');
 
+
 module.exports = function(app, config, errors) {
+  var controller = require('./controllers')(app, config, errors);
 
   // set views engine
   app.use(views(config.views.path, config.views.engine));
 
   // serve build files
   app.use(serve(app.directory + '/build'));
+
+  app.use(router.post('/todos', controller.todo.create));
+
+  app.use(router.get('/todos', controller.todo.list));
+
+  // app.use(router.post('/todos/:id', function *() {
+
+
+  // }));
 
   // render index
   app.use(router.get('/*', function *(next) {
