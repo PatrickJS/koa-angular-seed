7ae7f83e8af2dfdede97322089afc5ace0cae5e2
gdi2290 <github@gdi2290.com>
2014-02-01 01:55:52 -0800

    clean up routes.js

--- server/routes.js
+++ server/routes.js
@@ -2,18 +2,20 @@ var router = require('koa-route');
 var views = require('koa-render');
 var serve = require('koa-static');
 
-module.exports = function(app) {
+module.exports = function(app, config, errors) {
+
   // set views engine
-  app.use(views('./server/views', 'jade'));
+  app.use(views(config.views.path, config.views.engine));
 
   // serve build files
-  app.use(serve(app.rootPath + '/build'));
+  app.use(serve(app.directory + '/build'));
 
   // render index
   app.use(router.get('/*', function *(next) {
-    this.body = yield this.render('index', {
+    var locals = {
       title: 'Example App'
-    });
+    };
+    this.body = yield this.render('index', locals);
   }));
 
 
