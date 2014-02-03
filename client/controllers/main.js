22a4471468697ed78e765e8058f6eb3322c06dce
gdi2290 <github@gdi2290.com>
2014-02-03 01:13:49 -0800

    update main controller

--- client/controllers/main.js
+++ client/controllers/main.js
@@ -1,19 +1,4 @@
-angular.module('ngKoaApp')
-.controller('TodoCtrl', function($scope, TodoService) {
-
-  $scope.todos = TodoService.get();
-
-  $scope.add = function(todo) {
-    TodoService.add(todo);
-    $scope.newTodo = {};
-  };
-
-  $scope.remove = function(todo) {
-    TodoService.remove(todo);
-  };
-
-  $scope.complete = function(todo) {
-    todo.complete = !todo.complete;
-  };
+angular.module('app.controllers')
+.controller('MainCtrl', function($scope) {
 
 });
