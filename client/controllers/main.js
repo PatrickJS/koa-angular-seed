00fe7fdc35a0280388d5eb6f1b55d8930726be08
PatrickJS <github@gdi2290.com>
2014-02-03 00:55:53 -0800

    Update main.js

--- client/controllers/main.js
+++ client/controllers/main.js
@@ -1,4 +1,19 @@
 angular.module('ngKoaApp')
-.controller('MainController', function($scope) {
-  $scope.text = 'Angular.js';
+.controller('TodoCtrl', function($scope, TodoService) {
+
+  $scope.todos = TodoService.get();
+
+  $scope.add = function(todo) {
+    TodoService.add(todo);
+    $scope.newTodo = {};
+  };
+
+  $scope.remove = function(todo) {
+    TodoService.remove(todo);
+  };
+
+  $scope.complete = function(todo) {
+    todo.complete = !todo.complete;
+  };
+
 });
