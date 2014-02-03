3723a81eaa5c3dd85fe50ed4242c78792aa62e69
gdi2290 <github@gdi2290.com>
2014-02-03 01:13:55 -0800

    add todo controller

---
+++ client/controllers/todo.js
@@ -0,0 +1,19 @@
+angular.module('app.controllers')
+.controller('TodoCtrl', function($scope, TodoService) {
+
+  $scope.todos = TodoService.get();
+
+  $scope.add = function(todo) {
+    TodoService.add(todo);
+    $scope.newTodo = {};
+  };
+
+  $scope.remove = function(todo) {
+    TodoService.remove(todo);
+  };
+
+  $scope.complete = function(todo) {
+    todo.complete = !todo.complete;
+  };
+
+});
